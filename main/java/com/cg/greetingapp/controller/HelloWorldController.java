0e5399f9d7b072b0953ff9e4247805b233eb1819
Abhinav9p16 <thakurabhinav56@gmail.com>
2020-11-24 20:47:43 +0530

    Added: Hello messages using Spring

--- main/java/com/cg/greetingapp/controller/HelloWorldController.java
+++ main/java/com/cg/greetingapp/controller/HelloWorldController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/hello")
 public class HelloWorldController {
-   /* @RequestMapping(value = {"","/","/home"})
+   @RequestMapping(value = {"","/","/home"})
     public String sayHello(){
         return "Hello World!!!";
     }
@@ -25,5 +25,5 @@ public class HelloWorldController {
     @PutMapping("/put/{firstName}")
     public String sayHello(@PathVariable String firstName, @RequestParam(value = "lastName") String lastName){
         return "Hello " +firstName + " " + lastName + "!";
-    }*/
+    }
 }
