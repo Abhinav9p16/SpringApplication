0a7ad74885b3d404971363b9c8ca18d00f271b07
Abhinav9p16 <thakurabhinav56@gmail.com>
2020-11-24 20:46:21 +0530

    Welcome to Spring Application

---
+++ main/java/com/cg/greetingapp/GreetingAppApplication.java
@@ -0,0 +1,15 @@
+package com.cg.greetingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreetingAppApplication.class, args);
+    }
+
+}
+//curl -X PUT -H "Content-Type: application/json" -d '{"firstName":"Abhinav","lastName":"Thakur"}' "http://localhost:8080/hello/put/{Abhinav}"
+//curl -X PUT "http://localhost:8080/hello/put/Abhinav?lastName=Thakur"
---
+++ test/java/com/cg/greetingapp/GreetingAppApplicationTests.java
@@ -0,0 +1,12 @@
+package com.cg.greetingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
